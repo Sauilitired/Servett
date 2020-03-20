dcad72df18b73a15d1e9ae0bdcfc17879882c899
Alexander Söderberg <sauilitired@gmail.com>
2020-03-20 21:24:52 +0100

    Update upstream.sh.

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -1,28 +1,65 @@
 #!/usr/bin/env bash
+# get base dir regardless of execution location
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+. $(dirname $SOURCE)/init.sh
+
+git submodule update --init --recursive
+
+if [[ "$1" == up* ]]; then
+    (
+        cd "$basedir/Tuinity/"
+        git fetch && git reset --hard origin/beta
+        cd ../
+        git add Tuinity
+    )
+fi
+
+tuinityVer=$(gethead Tuinity)
+cd "$basedir/Tuinity/"
+
+chmod +x tuinity
+chmod +x scripts/*
+./tuinity patch
+
+cd "Tuinity-Server"
+mcVer=$(mvn -o org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=minecraft_version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }')
+
+basedir
+. $basedir/scripts/importmcdev.sh
+
+minecraftversion=$(cat $basedir/Tuinity/Paper/work/BuildData/info.json | grep minecraftVersion | cut -d '"' -f 4)
+version=$(echo -e "Tuinity: $tuinityVer\nmc-dev:$importedmcdev")
+tag="${minecraftversion}-${mcVer}-$(echo -e $version | shasum | awk '{print $1}')"
+echo "$tag" > $basedir/current-tuinity
+
+$basedir/scripts/generatesources.sh
+
+cd Tuinity/
+
+function tag {
 (
-set -e
-PS1="$"
-
-function changelog() {
-    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
-    cd $1 && git log --oneline ${base}...HEAD
+    cd $1
+    if [ "$2" == "1" ]; then
+        git tag -d "$tag" 2>/dev/null
+    fi
+    echo -e "$(date)\n\n$version" | git tag -a "$tag" -F - 2>/dev/null
+)
 }
-tuinity=$(changelog Tuinity)
+echo "Tagging as $tag"
+echo -e "$version"
 
-updated=""
-logsuffix=""
-if [ ! -z "$tuinity" ]; then
-    logsuffix="$logsuffix\n\nTuinity Changes:\n$tuinity"
-    if [ -z "$updated" ]; then updated="Tuinity"; else updated="$updated/Tuinity"; fi
-fi
-disclaimer="Upstream has released updates that appears to apply and compile correctly"
-
-if [ ! -z "$1" ]; then
-    disclaimer="$@"
+forcetag=0
+if [ "$(cat $basedir/current-tuinity)" != "$tag" ]; then
+    forcetag=1
 fi
 
-log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+tag Tuinity-API $forcetag
+tag Tuinity-Server $forcetag
 
-echo -e "$log" | git commit -F -
-
-) || exit 1
+pushRepo Tuinity-API $PAPER_API_REPO $tag
+pushRepo Tuinity-Server $PAPER_SERVER_REPO $tag
